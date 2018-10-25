f22eaea10f16244b8d6e367c80ca10bd105ff37b
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-25 23:49:57 +0900

    implemented functions for clearing errors upon registration and loading

--- slack/src/components/Auth/Register.js
+++ slack/src/components/Auth/Register.js
@@ -9,7 +9,8 @@ class Register extends React.Component {
         email: "",
         password: "",
         passwordConfirmation: "",
-        errors: []
+        errors: [],
+        loading: false
     }
     isFormValid = () => {
         let errors = []
@@ -44,21 +45,27 @@ class Register extends React.Component {
         this.setState({[event.target.name]: event.target.value })
     }
     handleSubmit = event => {
+        event.preventDefault();
         if (this.isFormValid()) {
-            event.preventDefault();
+            this.setState({errors: [], loading: true})
             firebase
                 .auth()
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser)
+                    this.setState({loading: false})
                 })
                 .catch(err => {
                     console.error(err)
+                    this.setState({errors: this.state.errors.concat(err), loading: false})
                 })
         }
     }
+    handleInputError = (errors, inputName) => {
+        return (errors.some(error => error.message.toLowerCase().includes(inputName)) ? "error" : "")
+    }
     render() {
-        const { username, email, password, passwordConfirmation, errors} = this.state
+        const { username, email, password, passwordConfirmation, errors, loading} = this.state
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -68,11 +75,11 @@ class Register extends React.Component {
                     </Header>
                     <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
-                            <Form.Input fluid name ="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} value={username} type="text" />
-                            <Form.Input fluid name ="email" icon="mail" iconPosition="left" placeholder="Email" onChange={this.handleChange} value={email} type="email" />
-                            <Form.Input fluid name ="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} value={password} type="password" />
-                            <Form.Input fluid name ="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={this.handleChange} value={passwordConfirmation} type="password" />
-                            <Button color="orange" fluid size="large">Submit</Button>
+                            <Form.Input fluid name ="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} value={username} className={this.handleInputError(errors, "username")} type="text" />
+                            <Form.Input fluid name ="email" icon="mail" iconPosition="left" placeholder="Email" onChange={this.handleChange} value={email} className={this.handleInputError(errors, "email")} type="email" />
+                            <Form.Input fluid name ="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} value={password} className={this.handleInputError(errors, "password")} type="password" />
+                            <Form.Input fluid name ="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={this.handleChange} value={passwordConfirmation} className={this.handleInputError(errors, "password")} type="password" />
+                            <Button disabled={loading} className={loading ? "loading" : ""} color="orange" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
                     {errors.length > 0 && (
