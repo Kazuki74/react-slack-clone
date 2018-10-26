700ab402ce34b6d0b332574298382f5aeab35728
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-26 15:36:30 +0900

    set up redux

--- slack/src/index.js
+++ slack/src/index.js
@@ -6,15 +6,24 @@ import Login from './components/Auth/Login';
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase';
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import withRouter from 'react-router-dom/withRouter';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools())
 
 class Root extends React.Component {
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                console.log(user)
+                this.props.setUser(user)
                 this.props.history.push('/')
             }
         })
@@ -30,12 +39,14 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
 
