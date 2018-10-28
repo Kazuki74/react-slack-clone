76e4cd923b2e85c6c84ed2b95e371fc5c2c6c57b
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-28 17:06:28 +0900

    Create Messages Collection, Add First Message to Database

--- slack/src/components/Messages/Messages.js
+++ slack/src/components/Messages/Messages.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
+import firebase from '../../firebase';
 import MessagesForm from './MessagesForm'
 import MessagesHeader from './MessagesHeader';
 
 class Messages extends React.Component {
+    state = {
+        messagesRef: firebase.database().ref('messages'),
+        channel: this.props.currentChannel,
+        user: this.props.currentUser
+    }
     render() {
+        const { messagesRef, channel, user } = this.state;
         return (
             <React.Fragment>
                 <MessagesHeader />
@@ -14,7 +21,11 @@ class Messages extends React.Component {
                     </Comment.Group>
                 </Segment>
 
-                <MessagesForm />
+                <MessagesForm 
+                    messagesRef={ messagesRef }
+                    currentChannel={ channel }
+                    currentUser={ user }
+                />
             </React.Fragment>
         )
     }
--- slack/src/components/Messages/MessagesForm.js
+++ slack/src/components/Messages/MessagesForm.js
@@ -1,21 +1,88 @@
 import React from 'react';
 import { Segment, Button, Input } from 'semantic-ui-react';
+import firebase from '../../firebase';
 
 class MessageForm extends React.Component {
+    state = {
+        message: '',
+        channel: this.props.currentChannel,
+        loading: false,
+        user: this.props.currentUser,
+        errors: []
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    createMessage = () => {
+        const message = {
+            timestamp: firebase.database.ServerValue.TIMESTAMP ,
+            user: {
+                id: this.state.user.uid,
+                name: this.state.user.displayName,
+                avatar: this.state.user.photoURL
+            },
+            content: this.state.message
+        }
+        return message;
+    }
+
+    sendMessage = () => {
+        const { messagesRef } = this.props;
+        const { message, channel } = this.state;
+        if (message) {
+            this.setState({ loading: true });
+            messagesRef
+                .child(channel.id)
+                .push()
+                .set(this.createMessage())
+                .then(() => {
+                    console.log("added a message!");
+                    this.setState({
+                        loading: false,
+                        message: '',
+                        errors: []
+                    })
+                })
+                .catch((err) => {
+                    this.setState({
+                        loading: false,
+                        errors: this.state.errors.concat(err)
+                    })
+                })
+        } else {
+            console.log('error!');
+            this.setState({
+                errors: this.state.errors.concat({
+                    message: "Add a message."
+                })
+            })
+        }
+    }
+
     render () {
+        const { errors } = this.state;
         return (
             <Segment className="message__form">
                 <Input 
                     fluid
                     name="message"
+                    onChange={this.handleChange}
                     style={{ marginBottom: '0.7em' }}
                     label={ <Button icon={"add"} />}
                     labelPosition="left"
+                    className={
+                        errors.some(error => error.message.includes('message')) ? "error" : ""
+                    }
                     placeholder="Write your message"
                 />
                 <Button.Group iconwidth="2" >
                     <Button
                         color="orange"
+                        onClick={this.sendMessage}
                         content="Add Reply"
                         labelPosition="left"
                         icon="edit"
