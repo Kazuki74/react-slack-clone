3cad51069e79c9314782896133e2a654140edc8e
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-08 00:58:48 +0900

    Add Color Picker Modal to ColorPanel.

--- slack/src/components/ColorPanel/ColorPanel.js
+++ slack/src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Sidebar, Divider, Button, Menu} from 'semantic-ui-react';
+import { Sidebar, Divider, Button, Menu, Modal, Icon, Label} from 'semantic-ui-react';
+import { SliderPicker } from 'react-color';
 
 class ColorPanel extends React.Component {
+    state = {
+        modal: false,
+    }
+
+    openModal = () => this.setState({ modal: true })
+
+    closeModal = () => this.setState({ modal: false })
+
     render() {
+        const { modal } = this.state;
         return (
             <Sidebar
                 as={Menu}
@@ -12,8 +22,27 @@ class ColorPanel extends React.Component {
                 visible
                 width="very thin"
             >
-            <Divider />
-            <Button icon="add" size="small" color="blue" />
+                <Divider />
+                <Button icon="add" size="small" color="blue" onClick={this.openModal}/>
+                <Modal basic open={modal} onClose={this.closeModal}>
+                    <Modal.Header>
+                        Choose App Color
+                    </Modal.Header>
+                    <Modal.Content>
+                        <Label content="Primary Color"/>
+                        <SliderPicker />
+                        <Label content="Secondary Color"/>
+                        <SliderPicker />
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="green" inverted>
+                            <Icon name="checkmark"/> Save Colors
+                        </Button>
+                        <Button color="red" inverted onClick={this.closeModal}>
+                            <Icon name="remove"/> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
             </Sidebar>
         )
     }
