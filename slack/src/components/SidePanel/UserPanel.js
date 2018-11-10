487bce41ed054376685d28bab4453e7cfa8295a8
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-10 12:06:49 +0900

    Implemented Chanege Avatar Modal.

--- slack/src/components/SidePanel/UserPanel.js
+++ slack/src/components/SidePanel/UserPanel.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image, Modal, Input, Button } from 'semantic-ui-react';
 import firebase from '../../firebase';
 // import {connect} from 'react-redux';
 
 class UserPanel extends React.Component {
     state = {
-        user: this.props.currentUser
+        user: this.props.currentUser,
+        modal: false,
     }
 
     dropdownOptions = () => [
@@ -16,13 +17,18 @@ class UserPanel extends React.Component {
         },
         {
             key: "avatar",
-            text: <span>Change Avatar</span>
+            text: <span onClick={this.openModal}>Change Avatar</span>
         },
         {
             key: "signout",
             text: <span onClick={this.handleSignout}>Sign Out</span>
         }
     ]
+
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
+
     handleSignout = () => {
         firebase
             .auth()
@@ -32,7 +38,7 @@ class UserPanel extends React.Component {
             })
     }
     render() {
-        const { user } = this.state;
+        const { user, modal } = this.state;
         const { primaryColor } = this.props;
         return(
             <Grid style={{ background: primaryColor}}>
@@ -54,6 +60,39 @@ class UserPanel extends React.Component {
                             />
                         </Header>
                     </Grid.Row>
+                    {/* Change User Avatar Modal */}
+                    <Modal basic open={modal} onClose={this.closeModal}>
+                        <Modal.Header>Change Avatar</Modal.Header>
+                        <Modal.Content>
+                            <Input 
+                                fluid
+                                type="file"
+                                label="New Avatar"
+                                name="previewImage"
+                            />
+                            <Grid centered stackable columns={2}>
+                                <Grid.Row centered>
+                                    <Grid.Column className="ui center aligned grid">
+                                        {/* Image Preview */}
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        {/* Cropped Image Preview */}
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color="green" inverted>
+                                <Icon name="save"/> Change Avatar
+                            </Button>
+                            <Button color="green" inverted>
+                                <Icon name="image"/> Preview
+                            </Button>
+                            <Button color="red" inverted onClick={this.closeModal}>
+                                <Icon name="remove"/> Cancel
+                            </Button>
+                        </Modal.Actions>
+                    </Modal>
                 </Grid.Column>
             </Grid>
         )
